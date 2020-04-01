988311b40c81a38e5bfe5e8ed2f1c8e9c88e1054
cinun <43857696+cinun@users.noreply.github.com>
2020-04-01 14:22:33 -0500

    First Commit
    Not tested when there are multiple reports to fill.

---
+++ FormComplete.py
@@ -0,0 +1,95 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+import time
+from sys import argv
+
+
+EMAIL, PASSWORD = argv[1].strip(), argv[2].strip()
+
+
+driver = webdriver.Chrome('C:/Python38/chromedriver/chromedriver.exe')
+
+
+#chrome_options = Options()
+#chrome_options.add_argument("--incognito")
+def login():
+    
+    driver.get("https://strive-ttu.campus.eab.com/home/staff")
+    #driver.set_window_size(1200,1200)
+
+    userName = driver.find_element_by_id("userNameInput")
+    passWord = driver.find_element_by_id("passwordInput")
+
+    #Perform login
+
+    userName.send_keys(EMAIL)
+    passWord.send_keys(PASSWORD)
+    passWord.send_keys(Keys.RETURN)
+# Go to staff profile and select each element
+
+
+def fillForm(final, rowCount):
+    '''
+    Select the student ID
+    Click on Actions and Add appointment summary
+    Fill out details
+    '''
+    #Step 1
+    driver.find_element_by_xpath(final+"[{}]/td[1]".format(rowCount)).click()
+
+    #Step 2
+    
+    action = driver.find_element_by_xpath("//*[@id=\"recent_appointments\"]/div[3]/div[1]/a").click()
+
+    driver.find_element_by_xpath("//*[@id=\"action-list-name--1\"]/div[1]/a").click()
+    
+    time.sleep(7)  #Wait for the iframe to load
+
+    #Step 3
+    
+    homework = driver.find_element_by_xpath("//*[@id=\"evaluation_assignment_description\"]")
+    homework.send_keys("Homework")
+
+    driver.switch_to.frame(driver.find_element_by_xpath("//*[@id=\"evaluation_comment_ifr\"]"))
+    
+    elem_iframe = driver.find_element_by_xpath("/html/body/p")
+    elem_iframe.send_keys("Assisted student with their academic needs during their visit.")
+
+    driver.switch_to.default_content()
+
+    #Submit the form
+
+    driver.find_element_by_xpath("//*[@id=\"submit_button\"]").click()
+    
+    
+
+    
+def checkBox():
+    
+    final = "//*[@id=\"recent_appointments\"]/div[4]/table/tbody/tr"
+    check_id = driver.find_elements_by_xpath(final)     
+    print(len(check_id))
+    value = len(check_id) + 1
+    for i in range(1, value):
+        if (driver.find_element_by_xpath(final+"[{}]/td[9]".format(i)).get_attribute("class") == "c"):
+            #Start filling the form
+            print("Incomplete report found")
+            fillForm(final, i)
+        else:
+            print ("Report Completed")
+
+    #driver.close()
+
+def main():
+    login()
+    checkBox()
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
